f5cc069e8a25186399d40c95e686e3ff54209375
Mrk-Cstle <109070381+Mrk-Cstle@users.noreply.github.com>
2025-05-25 12:05:20 +0800

    migration fix and userdetail view
    remove qr and face recognition in admin user view

--- resources/views/admin/viewuserdetails.blade.php
+++ resources/views/admin/viewuserdetails.blade.php
@@ -30,41 +30,7 @@
         </div>
     </div>
     <div class="flex flex-col  basis-[70%] gap-5">
-        <div class=" rounded-md grow-1 bg-white flex flex-row gap-3 p-5">
-            <div class="basis-[50%] border">
-                QR
-            </div>
-            <div class="basis-[50%] border flex flex-col">
-                <div class="flex flex-row justify-between m-3">
-                   
-                    <p>Face Recognition</p><button id="removeFaceToken" class="bg-[#FF0000] p-1 rounded-sm text-white">Remove</button>
-                </div>
-              
-                <div id="loadingSpinner" class="hidden fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-[9999]">
-                    <div class="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-blue-500"></div>
-                </div>
-               
-                <br>
-                @if(Auth::user()->face_token !== null && Auth::user()->face_token !== "")
-                <button id="capturemodal" class="px-4 m-5 py-2 bg-blue-200 text-white rounded" disabled>Capture & Register</button>
-                @else
-                    <button id="capturemodal" class="px-4  m-5 py-2 bg-blue-500 text-white rounded" >Capture & Register</button>
-                @endif
-                
-
-            <!-- Modal -->
-                    <div id="modal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 hidden z-50">
-                        <div class="bg-white p-6 rounded shadow-lg w-96">
-                            <h2 class="text-lg font-bold mb-4">Capture & Register</h2>
-                            <canvas id="canvas" width="320" height="240" style="display:none;"></canvas>
-                            <video id="video" width="320" height="240" autoplay></video>
-                            <button class="mt-4 px-4 py-2 bg-green-500 text-white rounded" id="capture">Capture</button>
-                            <button id="closemodal" class="mt-4 px-4 py-2 bg-red-500 text-white rounded">Close</button>
-                        </div>
-                    </div>
-             
-            </div>
-        </div>
+        
         <div class=" rounded-md grow-1 bg-white">
             <div class="basis-[70%]  flex flex-col p-5 overflow-y-auto">
                 <form id="updateProfile" class="flex flex-col gap-3" >
